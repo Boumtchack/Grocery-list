a20a46dd80f46628120f628795e5290145b3d20d
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-09 12:13:27 +0200

    added products dependencies to List

--- mygrocerylist/models.py
+++ mygrocerylist/models.py
@@ -3,17 +3,10 @@ from uuid import uuid4
 
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=40)
-    ammount = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
-
 
 class List(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default="my new list")
 
     def __str__(self):
         return self.title
@@ -21,7 +14,10 @@ class List(models.Model):
     def get_absolute_url(self):
         return "/mygrocerylist/%s/" % self.id
 
-
-class ListManager(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    ammount = models.IntegerField(default=1)
     list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
