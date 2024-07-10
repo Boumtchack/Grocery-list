1de6091a52610206cbc2f78268ad6b7116a28a3f
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-10 16:33:58 +0200

    intialise angular and start dependence with django

---
+++ grocery-list-angular/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'grocery-list-angular';
+  ngOnInit() {
+    console.log('hello');
+    this.http.get(
+      '/mygrocerylist/api/v1/user/5159c2bd-9126-49ad-b039-ef527da138b9'
+    ).subscribe(result => console.log(result));
+  }
+  constructor(private http: HttpClient) {
+    // This service can now make HTTP requests via `this.http`.
+  }
+}
---
+++ grocery-list-angular/src/app/grocery-list/grocery-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-grocery-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './grocery-list.component.html',
+  styleUrl: './grocery-list.component.scss',
+})
+export class GroceryListComponent {
+
+}
