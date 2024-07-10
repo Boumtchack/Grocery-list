f08b07fd0717543e15ae829f79e848ff6e772eaf
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-10 17:25:12 +0200

    getting json data and displaying it in a template of component

--- grocery-list-angular/src/app/app.component.ts
+++ grocery-list-angular/src/app/app.component.ts
@@ -2,21 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'grocery-list-angular';
+  user: any = {};
   ngOnInit() {
-    console.log('hello');
-    this.http.get(
-      '/mygrocerylist/api/v1/user/5159c2bd-9126-49ad-b039-ef527da138b9'
-    ).subscribe(result => console.log(result));
+    this.http
+      .get('/mygrocerylist/api/v1/user/5159c2bd-9126-49ad-b039-ef527da138b9')
+      .subscribe((result) => {this.user = result});
   }
   constructor(private http: HttpClient) {
     // This service can now make HTTP requests via `this.http`.
