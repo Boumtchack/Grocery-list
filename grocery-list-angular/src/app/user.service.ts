dd1301cd8c7aee3da80b17107f3e142da256c281
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-12 14:22:30 +0200

    minor changes

--- grocery-list-angular/src/app/user.service.ts
+++ grocery-list-angular/src/app/user.service.ts
@@ -27,3 +27,14 @@ export class UserService {
     return this.userList.find(user => user.id === id)
   }
 }
+
+// export class UserComponent implements OnInit {
+//   user: any = {};
+//   ngOnInit() {
+//     this.http
+//       .get('/mygrocerylist/api/v1/user/5159c2bd-9126-49ad-b039-ef527da138b9')
+//       .subscribe((result) => {this.user = result});
+//   }
+//   constructor(private http: HttpClient) {
+//     // This service can now make HTTP requests via `this.http`.
+//   }
--- grocery-list-angular/src/app/user/user.component.ts
+++ grocery-list-angular/src/app/user/user.component.ts
@@ -16,13 +16,3 @@ export class UserComponent{
   @Input() user!:User;
 }
 
-// export class UserComponent implements OnInit {
-//   user: any = {};
-//   ngOnInit() {
-//     this.http
-//       .get('/mygrocerylist/api/v1/user/5159c2bd-9126-49ad-b039-ef527da138b9')
-//       .subscribe((result) => {this.user = result});
-//   }
-//   constructor(private http: HttpClient) {
-//     // This service can now make HTTP requests via `this.http`.
-//   }
