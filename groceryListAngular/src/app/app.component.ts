8d867273d81f105cf73ecaaf0de08e0427443091
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-24 18:08:11 +0200

    setup proxy

--- groceryListAngular/src/app/app.component.ts
+++ groceryListAngular/src/app/app.component.ts
@@ -10,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'groceryListAngular';
+  private url = '/mygrocerylist/api/v1/'
+  createUser(){
+    console.log("user created");
+  }
+
+  findUser(){
+    let ressource = 'user/'
+    fetch(this.url + ressource + '14aba67a-43e1-4eb1-88fd-8d5685da8ca2')
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+  }
 }
