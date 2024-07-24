998d4309a6ac5413abc5bc978d264babe23208a9
AntoineBoumBoum <haffreinguea+github@gmail.com>
2024-07-24 21:58:49 +0200

    can find and display user

--- groceryListAngular/src/app/app.component.ts
+++ groceryListAngular/src/app/app.component.ts
@@ -7,22 +7,26 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'groceryListAngular';
-  private url = '/mygrocerylist/api/v1/'
-  uuid =''
-  createUser(){
-    console.log("user created");
+  private url = '/mygrocerylist/api/v1/';
+  uuid = '';
+  newName = '';
+  currentUser = { id: '', name: '' };
+
+  createUser() {
+    console.log('user created');
   }
 
-  findUser(){
-    let ressource = 'user/'
-    fetch(this.url + ressource + this.uuid )
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-    })
+  findUser() {
+    let ressource = 'user/';
+    fetch(this.url + ressource + this.uuid)
+      .then((response) => response.json())
+      .then((data) => {
+        this.currentUser = data;
+        console.log(this.currentUser);
+      });
   }
 }
